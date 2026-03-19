a83e45b9acb2dbd30f9c75ac3e603f84d0e20c95
ajberkley <ajberkley@gmail.com>
2026-03-19 06:18:27 -0700

    add interactive example

--- src/cl-matplotlib.lisp
+++ src/cl-matplotlib.lisp
@@ -7,10 +7,14 @@
 (in-package :cl-matplotlib)
 
 (defun install-python-packages ()
-  (uiop:run-program '("pip" "install" "matplotlib")))
+  (uiop:run-program '("pip" "install" "matplotlib"))
+  (uiop:run-program '("pip" "install" "scipy")))
 
 (defun initialize ()
-  (py4cl2:pyexec "import matplotlib.pyplot as plt"))
+  (py4cl2:pyexec "import sys")
+  (py4cl2:pyexec "sys.path.insert(0, '.')")
+  (py4cl2:pyexec "import matplotlib.pyplot as plt")
+  (py4cl2:pyexec "from src import test_interactive_plot"))
 
 (defpyfun "plot" "matplotlib.pyplot" :lisp-fun-name "PLOT")
 (defpyfun "show" "matplotlib.pyplot" :lisp-fun-name "SHOW")
@@ -20,3 +24,12 @@
          (speed (mapcar (lambda (x) (* x x)) time)))
     (plot time speed ".-")
     (show)))
+
+(defun try-interactive-plot ()
+  (initialize)
+  (py4cl2:pyexec "from src import test_interactive_plot as test")
+  ;;(py4cl2:with-remote-objects
+  (let ((plt (py4cl2:pyeval "test.testPlot()")))
+    (py4cl2:pycall "test.testPlot.load_config" plt "src/config.yaml")
+    (py4cl2:pycall "test.testPlot.generate_data" plt)
+    (py4cl2:pycall "test.testPlot.make_plot" plt)))
