fbfae03ef1751088838c2a4f0753bc60ee270e41
ajberkley <ajberkley@gmail.com>
2026-03-20 08:20:41 -0700

    Add legend that can turn on and off

--- src/test_interactive_plot.py
+++ src/test_interactive_plot.py
@@ -165,8 +165,9 @@ class testPlot:
                                 zorder=5,
                                 label='Logistic',
                                 color=color_wheel[1])
-        self.legend = self.ax.legend(loc=2)
-        self.legend.set_draggable(True)
+        legend = self.ax.legend(loc=2)
+        legend.set_draggable(True)
+        self.legend = InteractiveLegend(self.ax.get_legend())
         # if hasattr(self, 'legend_pos'):
         #     self.legend.set_bbox_to_anchor(self.legend_pos.transformed(
         #         self.ax.transAxes.inverted()
@@ -192,3 +193,83 @@ class testPlot:
         self.redraw()
         plt.show(block=block)
         plt.pause(0.1)
+
+
+def interactive_legend(ax=None):
+    if ax is None:
+        ax = plt.gca()
+    if ax.legend_ is None:
+        ax.legend()
+
+    return InteractiveLegend(ax.get_legend())
+
+class InteractiveLegend(object):
+    def __init__(self, legend):
+        self.legend = legend
+        self.fig = legend.axes.figure
+
+        self.lookup_artist, self.lookup_handle = self._build_lookups(legend)
+        self._setup_connections()
+
+        self.update()
+
+    def _setup_connections(self):
+        for artist in self.legend.texts + self.legend.legend_handles:
+            artist.set_picker(10) # 10 points tolerance
+
+        self.fig.canvas.mpl_connect('pick_event', self.on_pick)
+        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+
+    def _build_lookups(self, legend):
+        labels = [t.get_text() for t in legend.texts]
+        handles = legend.legend_handles
+        label2handle = dict(zip(labels, handles))
+        handle2text = dict(zip(handles, legend.texts))
+
+        lookup_artist = {}
+        lookup_handle = {}
+        for artist in legend.axes.get_children():
+            if artist.get_label() in labels:
+                handle = label2handle[artist.get_label()]
+                lookup_handle[artist] = handle
+                lookup_artist[handle] = artist
+                lookup_artist[handle2text[handle]] = artist
+
+        lookup_handle.update(zip(handles, handles))
+        lookup_handle.update(zip(legend.texts, handles))
+
+        return lookup_artist, lookup_handle
+
+    def on_pick(self, event):
+        handle = event.artist
+        if handle in self.lookup_artist:
+            artist = self.lookup_artist[handle]
+            artist.set_visible(not artist.get_visible())
+            self.update()
+
+    def on_click(self, event):
+        if event.button == 3:
+            visible = False
+        elif event.button == 2:
+            visible = True
+        else:
+            return
+
+        for artist in self.lookup_artist.values():
+            artist.set_visible(visible)
+        self.update()
+
+    def update(self):
+        for artist in self.lookup_artist.values():
+            handle = self.lookup_handle[artist]
+            if artist.get_visible():
+                handle.set_visible(True)
+            else:
+                handle.set_visible(False)
+        self.fig.canvas.draw()
+
+    def get_window_extent(self):
+        return self.legend.get_window_extent()
+        
+    def show(self):
+        plt.show()
