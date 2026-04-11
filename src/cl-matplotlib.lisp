8f89e8071b23aa43aeb0d6bacdd03887e98075e2
ajberkley <ajberkley@gmail.com>
2026-04-11 11:44:08 -0700

    remove py4cl2 package prefixes

--- src/cl-matplotlib.lisp
+++ src/cl-matplotlib.lisp
@@ -17,24 +17,24 @@
 
 (defun start-up/internal ()
   (setf *loop-started* nil)
-  (py4cl2:pyexec (format nil "import sys; sys.path.insert(0, '~a')"
-			 (directory-namestring
-			  (asdf:component-pathname
-			   (asdf:find-component :py4cl2 "python-code")))))
-  (py4cl2:pyexec (format nil "import sys; sys.path.insert(0, '~a')"
-			 (directory-namestring
-			  (asdf:component-pathname
-			   (asdf:find-component :cl-matplotlib "python-code"))))))
+  (pyexec (format nil "import sys; sys.path.insert(0, '~a')"
+		  (directory-namestring
+		   (asdf:component-pathname
+		    (asdf:find-component :py4cl2 "python-code")))))
+  (pyexec (format nil "import sys; sys.path.insert(0, '~a')"
+		  (directory-namestring
+		   (asdf:component-pathname
+		    (asdf:find-component :cl-matplotlib "python-code"))))))
 
-(py4cl2:defpymodule "matplotlib.widgets" nil :lisp-package "WID")
-(py4cl2:defpymodule "matplotlib.pyplot" nil :lisp-package "PLT")
-(py4cl2:defpymodule "matplotlib" nil :lisp-package "MPL")
+(defpymodule "matplotlib.widgets" nil :lisp-package "WID")
+(defpymodule "matplotlib.pyplot" nil :lisp-package "PLT")
+(defpymodule "matplotlib" nil :lisp-package "MPL")
 
 (defun draw (ax event)
   (format t "Got event ~A~%" event)
-  (py4cl2:pymethod ax "plot"
-                   (loop repeat 3 collect (random 10))
-                   (loop repeat 3 collect (random 10))))
+  (pymethod ax "plot"
+            (loop repeat 3 collect (random 10))
+            (loop repeat 3 collect (random 10))))
 
 (defun try-callbacks ()
   (unless *loop-started* (start-loop))
@@ -42,11 +42,11 @@
   (plt:ion) ;; this is critical otherwise redrawing doesn't happen without a plt:pause call
   (destructuring-bind (fig ax)
       (plt:subplots)
-    (py4cl2:pymethod ax "plot" '(1 2 3) '(3 1 2))
-    (py4cl2:pymethod fig "subplots_adjust" :bottom 0.2)
-    (let* ((button-ax (py4cl2:pymethod fig "add_axes" '(0.7 0.05 0.1 0.075)))
+    (pymethod ax "plot" '(1 2 3) '(3 1 2))
+    (pymethod fig "subplots_adjust" :bottom 0.2)
+    (let* ((button-ax (pymethod fig "add_axes" '(0.7 0.05 0.1 0.075)))
            (button (pycall "matplotlib.widgets.Button" button-ax "boo")))
-      (py4cl2:pymethod button "on_clicked" (alexandria:curry 'draw ax)))
+      (pymethod button "on_clicked" (alexandria:curry 'draw ax)))
     (plt:pause :interval 0.001)
     (plt:show :block nil)))
 
@@ -55,19 +55,19 @@
   (start-up/internal)
   (py4cl2::raw-py-exec/no-return "import PyQt6_example; PyQt6_example.start_app(try_process_message);")
   ;; Verify that the system is OK.
-  (py4cl2:pyeval "1 + 1")
+  (assert (= (pyeval "1 + 1") 2))
   ;; The above will throw an error if the no-return statement did not succeed
   (setf *loop-started* t))
 
 (defun try-interactive-plot ()
   "Call me!"
   (unless *loop-started* (start-loop))
-  (py4cl2:pyexec "import matplotlib")
-  (py4cl2:pyexec "import test_interactive_plot as test")
-  (let ((plt (py4cl2:pyeval "test.testPlot()")))
-    (py4cl2:pycall "test.testPlot.load_config" plt "src/config.yaml")
-    (py4cl2:pycall "test.testPlot.generate_data" plt)
-    (values plt (py4cl2:pycall "test.testPlot.make_plot" plt nil))))
+  (pyexec "import matplotlib")
+  (pyexec "import test_interactive_plot as test")
+  (let ((plt (pyeval "test.testPlot()")))
+    (pycall "test.testPlot.load_config" plt "src/config.yaml")
+    (pycall "test.testPlot.generate_data" plt)
+    (values plt (pycall "test.testPlot.make_plot" plt nil))))
 
 ;; venv support
 ;;(setf (py4cl2:config-var 'py4cl2:pycmd) "/path/to/python/of/your/venv")
